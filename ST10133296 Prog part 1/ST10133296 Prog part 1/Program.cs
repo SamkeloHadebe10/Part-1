ba1e16c1349a84fb1c75e80b19caea990cb0b6c7
ST10133296 <105270713+SamkeloHadebe10@users.noreply.github.com>
2023-04-26 15:53:02 +0200

    Add files via upload

---
+++ ST10133296 Prog part 1/ST10133296 Prog part 1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10133296_Prog_part_1
+{
+    class Ingredient
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // new instance of the Recipe class
+                Recipe recipe = new Recipe();
+
+                // infinite loop that displays a menu of options to the user
+                while (true)
+                {
+                    Console.WriteLine("***********************************************\n" +
+                                     "Recipe Application\n" +
+                                     "***********************************************\n" +
+                                     "\n" +
+                                     "(A) Press (1) to enter the details of your recipe.\n" +
+                                    "(B) Press (2) to scale your recipe.\n" +
+                                     "(C) Press (3) to delete your recipe.\n" +
+                                     "(D) Press (4) to Reset to the initial values.\n" +
+                                     "(E) Press (5) to display the recipe.\n" +
+                                     "(F) Press (6) to exit the application.\n" +
+                                     "\n" +
+                                     "***********************************************\n");
+
+                    // Prompt the user to choose an option
+                    Console.WriteLine("Choose from list above to continue ");
+                    int choice = int.Parse(Console.ReadLine());
+
+                    // Based on the user's choice, a method will be called from the Recipe class 
+                    if (choice == 1)
+                    {
+                        recipe.EnterDetails();
+                    }
+
+                    else if (choice == 2)
+                    {
+                        Console.Write("Enter scaling factor: ");
+                        double factor = double.Parse(Console.ReadLine());
+                        recipe.ScaleRecipe(factor);
+                    }
+
+                    else if (choice == 3)
+                    {
+                        recipe.ClearData();
+                    }
+
+                    else if (choice == 4)
+                    {
+                        recipe.ResetQuantities();
+                    }
+
+                    else if (choice == 5)
+                    {
+                        recipe.DisplayRecipe();
+                    }
+
+                    else if (choice == 6)
+                    {
+                        // Exits the loop and ends the program
+                        break;
+                    }
+
+                }
+                }
+            }
+        }
+    }
+
+
---
+++ ST10133296 Prog part 1/ST10133296 Prog part 1/Recipe.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10133296_Prog_part_1
+{
+    class Recipe
+    {
+        // private fields to store recipe information
+        private int numIngredients;
+        private List<string> ingredientNames;
+        private List<double> ingredientQuantity;
+        private List<string> ingredientUnits;
+        private int numSteps;
+        private List<string> steps;
+
+        // Constructor that initializes the private fields as empty lists
+        public Recipe()
+        {
+            ingredientNames = new List<string>();
+            ingredientQuantity = new List<double>();
+            ingredientUnits = new List<string>();
+            steps = new List<string>();
+        }
+
+        // Method that prompts the user to enter details for the recipe and stores them in the private fields
+        public void EnterDetails()
+        {
+            Console.WriteLine("Enter the number of ingredients for your recipe ");
+            numIngredients = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"Enter the name of ingredient {i + 1}: ");
+                string name = Console.ReadLine();
+                ingredientNames.Add(name);
+
+                Console.WriteLine($"Enter the quantity of {name} ");
+                double quantity = double.Parse(Console.ReadLine());
+                ingredientQuantity.Add(quantity);
+
+                Console.WriteLine($"Enter the unit of measurement for {name} (l or ml or g or kg) ");
+                string unit = Console.ReadLine();
+                ingredientUnits.Add(unit);
+            }
+
+            Console.WriteLine("Enter the number of steps on your recipe ");
+            numSteps = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"Enter step {i + 1}");
+                string step = Console.ReadLine();
+                steps.Add(step);
+            }
+        }
+
+        // Method that displays the recipe in a formatted manner
+        public void DisplayRecipe()
+        {
+            Console.WriteLine("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" +
+                              " Ingredients on your Recipe\n" +
+                               "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"{ingredientQuantity[i]} {ingredientUnits[i]} {ingredientNames[i]}");
+            }
+
+            Console.WriteLine("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n" +
+                              "Steps to take on your Recipe\n" +
+                                "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"{i + 1}. {steps[i]}");
+            }
+        }
+
+        // Method that scales the recipe ingredients by a factor provided by the user
+        public void ScaleRecipe(double factor)
+        {
+            for (int i = 0; i < numIngredients; i++)
+            {
+                ingredientQuantity[i] *= factor;
+            }
+        }
+
+        // Method that resets the recipe ingredient quantities to half of their current values
+        public void ResetQuantities()
+        {
+            for (int i = 0; i < numIngredients; i++)
+            {
+                ingredientQuantity[i] /= 2;
+            }
+        }
+
+        // Method that clears all recipe data and resets all private fields to their default values
+        public void ClearData()
+        {
+            numIngredients = 0;
+            ingredientNames.Clear();
+            ingredientQuantity.Clear();
+            ingredientUnits.Clear();
+            numSteps = 0;
+            steps.Clear();
+        }
+    }
+}
